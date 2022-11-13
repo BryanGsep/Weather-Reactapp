14a912e7401d16c6239d97047ac77368d35c1c32
BryanGsep <tiendung.do1907@gmail.com>
2022-11-13 20:13:44 +0900

    Upload node module into react-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import Search from './components/search/search';
 import './App.css';
+import CurrentWeather from './components/current-weather/current-weather';
+import { WEATHER_API_URL, WEATHER_API_KEY } from './api';
+import { ForecastWeather } from './components/forecast-weather/forecast-weather';
 
 function App() {
+
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecastWeather, setForecastWeather] = useState(null)
+  
+  useEffect(() => {
+    if (currentWeather !== null) {
+      document.body.style.backgroundImage = `url(./images/${currentWeather.weather[0].icon}.png)`;
+    } else {
+      document.body.style.backgroundImage = "url(./images/01d.png)";
+    };
+  }, [currentWeather])
+
+  const handleOnSearchChange = (searchData) => {
+    const [lat, lon] = searchData.value.split(" ");
+    const currentWeather =  fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
+    const forecastWeather = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`);
+
+    Promise.all([currentWeather, forecastWeather])
+      .then( async (response) => {
+        const weatherResponse = await response[0].json();
+        const forecastResponse = await response[1].json();
+        
+        setCurrentWeather({city: searchData.label, ...weatherResponse});
+        setForecastWeather({meta: searchData.label, ...forecastResponse});
+      })
+      .catch(err => console.error(err));
+  }
+
+  console.log(currentWeather)
+  console.log(forecastWeather)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Search onSearchChange={handleOnSearchChange}/>
+      {currentWeather && <CurrentWeather data={currentWeather}/>}
+      {forecastWeather && <ForecastWeather data={forecastWeather}/>}
     </div>
   );
 }
---
+++ src/components/current-weather/current-weather.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./current-weather.css"
+
+
+
+var CurrentWeather = ({data}) => {
+
+    return (
+        <div className="weather">
+            <div className="top">
+                <div>
+                    <p className="city"> {data.city} </p>
+                    <p className="weather-description">{data.weather[0].description}</p>
+                </div>
+                <img alt="weather" className="weather-icon" src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`} width="50px"/>
+            </div>
+            <div className="bottom">
+                <p className="temperature">{Math.round(data.main.temp)}°C</p>
+                <div className="details">
+                    <div className="parametter-row">
+                        <span className="parameter-label top">Details</span>
+                    </div>
+                    <div className="parameter-row">
+                        <span className="parameter-label">Feels like: </span>
+                        <span className="parameter-value">{Math.round(data.main.feels_like)}°C</span>
+                    </div>
+                    <div className="parameter-row">
+                        <span className="parameter-label">Wind: </span>
+                        <span className="parameter-value">{data.wind.speed}m/s</span>
+                    </div>
+                    <div className="parameter-row">
+                        <span className="parameter-label">Humidity: </span>
+                        <span className="parameter-value">{data.main.humidity}%</span>
+                    </div>
+                    <div className="parameter-row">
+                        <span className="parameter-label">Pressure: </span>
+                        <span className="parameter-value">{Math.round(data.main.pressure/100)}hPa</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default CurrentWeather;
